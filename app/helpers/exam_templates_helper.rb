6b560943e2f96f1846ab0216ad3860d3fa46fdc4
jenachoi <jena.choi1106@gmail.com>
2017-06-01 16:55:51 -0400

    adding helper method for sending template division data

---
+++ app/helpers/exam_templates_helper.rb
@@ -0,0 +1,23 @@
+module ExamTemplatesHelper
+  def get_template_divisions_table_info
+    tags = E.order(:name)
+
+    tags.map do |tag|
+      t = tag.attributes
+      t[:user_name] = User.find(tag.user).first_name +
+        ' ' + User.find(tag.user).last_name
+      t[:use] = get_num_groupings_for_tag(tag.id)
+      t[:edit_link] = view_context.link_to(
+        'Edit',
+        edit_tag_dialog_assignment_tag_path(@assignment, tag),
+        remote: true)
+      t[:delete_link] = view_context.link_to(
+        'Delete',
+        controller: 'tags',
+        action: 'destroy',
+        data: { confirm: 'Are you sure you want to delete this tag?' },
+        id: tag.id)
+      t
+    end
+  end
+end
