340299183b56d5644e57508f37fffa00a185bfcf
jenachoi <jena.choi1106@gmail.com>
2017-06-01 17:08:02 -0400

    modified helper function

--- app/helpers/exam_templates_helper.rb
+++ app/helpers/exam_templates_helper.rb
@@ -1,6 +1,6 @@
 module ExamTemplatesHelper
   def get_template_divisions_table_info
-    template_divisions = @exam_templates.template_divisions
+    template_divisions = @exam_templates[0].template_divisions
 
     template_divisions.map do |division|
       t = division.attributes
